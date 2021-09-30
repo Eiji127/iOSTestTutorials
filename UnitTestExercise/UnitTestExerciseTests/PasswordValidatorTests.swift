6a657506ad06ba7534fc2332f7581116394eec1c
Eiji <64912886+Eiji127@users.noreply.github.com>
2021-09-30 19:12:36 +0900

    [Update]: add PasswordValidatorTests class

--- UnitTestExercise/UnitTestExerciseTests/PasswordValidatorTests.swift
+++ UnitTestExercise/UnitTestExerciseTests/PasswordValidatorTests.swift
@@ -9,5 +9,38 @@ import XCTest
 @testable import UnitTestExercise
 
 class PasswordValidatorTests: XCTestCase {
+    // MARK: - 8文字以上であること
+    func test_数字が2文字含まれており_合計7文字入力された場合にfalseが返されること() {
+        XCTAssertFalse(validate(password: "abcde12"))
+    }
     
+    func test_数字が2文字含まれており_合計8文字入力された場合にtrueが返されること() {
+        XCTAssertTrue(validate(password: "abcdef12"))
+    }
+    
+    func test_数字が2文字含まれており_合計9文字入力された場合にtrueが返されること() {
+        XCTAssertTrue(validate(password: "abcdefg12"))
+    }
+    
+    // MARK: - 数字が2文字以上利用されていること
+    func test_数字以外を7文字と_合数字が1文字入力された場合にfalseが返されること() {
+        XCTAssertFalse(validate(password: "abcdefg1"))
+    }
+    
+    func test_数字以外を7文字と_合数字が2文字入力された場合にtrueが返されること() {
+        XCTAssertTrue(validate(password: "abcdefg12"))
+    }
+    
+    func test_数字以外を7文字と_合数字が3文字入力された場合にtrueが返されること() {
+        XCTAssertTrue(validate(password: "abcdefg123"))
+    }
+    
+    
+    func validate(password: String) -> Bool {
+        if password.count <= 7 {
+            return false
+        }
+        let numString = password.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+        return numString.count >= 2
+    }
 }
